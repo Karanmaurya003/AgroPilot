95d2ada860bc2ea225729a825c6ee0a3be35af81
Karanmaurya003 <karanaim11@gmail.com>
2025-10-30 10:18:15 +0530

    redoplyed the correct version

---
+++ windows/cl_wrapper.bat
@@ -0,0 +1,2 @@
+@echo off
+"C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/bin/Hostx64/x64/cl.exe" %*
---
+++ windows/msvc_toolchain.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_NAME Windows)
+
+# Use forward slashes to avoid invalid escape sequences
+set(CMAKE_C_COMPILER "E:/Mini_Project/AgroPilot/windows/cl_wrapper.bat")
+set(CMAKE_CXX_COMPILER "E:/Mini_Project/AgroPilot/windows/cl_wrapper.bat")
